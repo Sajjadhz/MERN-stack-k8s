db4bc99d239854fc9d65888266b2cb8280008009
Sajjad Hosseinzadeh <sajjad.hoss@mtnirancell.ir>
2023-11-13 19:08:12 +0330

    first commit

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl apply -f cloudl-client-service.yml
 kubectl apply -f cloudl-client-deployment.yml
 
 kubectl get pods
--- k8s/undeploy.sh
+++ k8s/undeploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 kubectl delete deploy/cloudl-client-deployment
-kubectl delete svc/cloudl-client-service
 
 kubectl get pods
